586ab1688e0079465ba0541abefc414d3d6d7867
gustavocadev <gustavocadev@gmail.com>
2024-02-07 10:16:08 -0500

    Refactor Header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,14 +1,14 @@
-import Link from "next/link"
-import Hamburger from "hamburger-react"
-import { useState } from "react"
+import Link from 'next/link';
+import Hamburger from 'hamburger-react';
+import { useState } from 'react';
 
 const Header = () => {
-  const [isOpen, setOpen] = useState(false)
+  const [isOpen, setOpen] = useState(false);
 
   return (
-    <header className="text-white ">
+    <header className="text-white  py-4">
       <nav className="flex flex-col items-center justify-between w-10/12 mx-auto sm:flex-row h-[92px]">
-        <div className="flex items-center justify-between w-full h-full ">
+        <div className="flex items-center justify-between w-full h-full">
           <figure className="relative z-50">
             <img src="/assets/svg/clover.svg" alt="This is a clover image" />
           </figure>
@@ -19,28 +19,22 @@ const Header = () => {
 
         <ul
           className={`${
-            isOpen ? "flex" : "hidden"
+            isOpen ? 'flex' : 'hidden'
           } sm:flex flex-col sm:flex-row items-center font-semibold text-[18px] sm:static absolute top-0 bg-[#212529] sm:bg-transparent w-full sm:w-auto pt-24 sm:pt-0`}
         >
           <li>
-            <Link href="/">
-              <a className="">Home</a>
-            </Link>
+            <Link href="/">Home</Link>
           </li>
           <li className="px-[56px] py-[20px] sm:py-0">
-            <Link href="/blog">
-              <a>Blog</a>
-            </Link>
+            <Link href="/blog">Blog</Link>
           </li>
           <li className="pb-[16px] sm:pb-0">
-            <Link href="/about">
-              <a>About</a>
-            </Link>
+            <Link href="/about">About</Link>
           </li>
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
